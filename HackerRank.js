dc3cc2a8fe5e4b4b300a136a0e509dba98bdfe4c
Dalvin Cash <cashdalvin1@gmail.com>
2020-09-16 11:28:05 -0500

    sept16Push2

--- HackerRank.js
+++ HackerRank.js
@@ -124,3 +124,20 @@ function arrayDiff(a, b) {
   }
   return difArray
 }
+
+
+//A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+function isPangram(string){
+  const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+                    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                    'u','v', 'w', 'x', 'y', 'z'];
+  let result = [];
+  string.toLowerCase().replace(/\w/g, (str) => {
+    str.split('').reduce((acc, cur) => {
+      return !result.includes(cur)
+         && alphabet.includes(cur)
+         && result.push(cur);
+      }, '');
+   });
+  return alphabet.join('') === result.sort().join('');
+}
