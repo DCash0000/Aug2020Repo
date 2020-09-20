226f5cdb95a1df4b30ea1f40bbd85abdb6f09161
Dalvin Cash <cashdalvin1@gmail.com>
2020-09-19 23:12:19 -0500

    push2

--- HackerRank.js
+++ HackerRank.js
@@ -153,3 +153,14 @@ function createPhoneNumber(numbers){
   
   return format;
 }
+
+//Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+function humanReadable(seconds) {
+  var hours = seconds / 3600, minutes = seconds / 60 % 60, newSeconds = seconds %  60 ;
+  return formatDate(hours) +':' + formatDate(minutes)+':'+formatDate(newSeconds)
+}
+
+function formatDate(n){
+  var number = Number.parseInt(n)
+  return number > 9? number : '0'+number;
+}
