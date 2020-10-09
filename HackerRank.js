e84380704552857e30df083cc057c92bb510c6c2
Dalvin Cash <cashdalvin1@gmail.com>
2020-10-09 14:17:48 -0500

    push

--- HackerRank.js
+++ HackerRank.js
@@ -261,3 +261,18 @@ function domainName(url){
   }
 }
 
+//Given an n x n array, return the array elements arranged from outermost elements to the middle element, traveling clockwise.
+function snail(array){
+  const sorted = []
+  while(array.length){
+    sorted.push(...array.shift())
+    for(let i=0; i<array.length; i++){
+      sorted.push(array[i].pop())
+    }
+    sorted.push(...(array.pop() || []).reverse())
+    for(let i=array.length-1; i>=0; i--){
+      sorted.push(array[i].shift())
+    }
+  }
+  return sorted
+}
