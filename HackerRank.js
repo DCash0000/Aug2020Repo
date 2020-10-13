e2f65e0bb2431d34d66766188693df0ec645e4c2
Dalvin Cash <cashdalvin1@gmail.com>
2020-10-12 19:41:15 -0500

    push

--- HackerRank.js
+++ HackerRank.js
@@ -291,3 +291,14 @@ function sum_pairs(ints, s){
   return undefined;
 
 }
+
+//Sheldon, Leonard, Penny, Rajesh and Howard are in the queue for a "Double Cola" drink vending machine; there are no other people in the queue. The first one in the queue (Sheldon) buys a can, drinks it and doubles! The resulting two Sheldons go to the end of the queue. Then the next in the queue (Leonard) buys a can, drinks it and gets to the end of the queue as two Leonards, and so on.
+function whoIsNext(names, r){
+  //your code here
+   r = r - 1; 
+
+  while (r >= names.length) {
+    r = Math.floor((r - names.length) / 2);
+  }
+  return names[r];
+}
